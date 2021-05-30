ee53ee803fefb84bd8766be84dc3eb6437c4ed9e
Gareth Howell <gareth.howell@gmail.com>
2021-05-30 13:24:09 +0100

    project setup

--- setup.py
+++ setup.py
@@ -1,8 +1,34 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+
+import io
+import re
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
 from setuptools import setup, find_packages
 
-setup(name="race_state", packages=find_packages())
+def read(*names, **kwargs):
+    with io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ) as fh:
+        return fh.read()
 
-extras_require={
+setup(
+    name="race_state", 
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    extras_require={
     "test": ["coverage", "mypy", "pycodestyle", "pytest", "pytest-cov",
-    "pytest-mock", "pytest-asyncio"]
-}
+    "pytest-mock", "pytest-asyncio"]},
+    setup_requires=[
+        'pytest-runner',
+    ],
+)
